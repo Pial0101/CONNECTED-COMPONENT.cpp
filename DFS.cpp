9d3a969bfdf3deb08c46ab708437e202eb48bd5b
Pial0101 <152403984+Pial0101@users.noreply.github.com>
2024-05-21 02:33:50 +0600

    Add files via upload

---
+++ DFS.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int MAX=1e5+10;
+vector<ll> g[MAX];
+bool vis[MAX];
+ 
+ 
+void dfs(ll vertex) {
+    vis[vertex] = true;
+    for(ll child : g[vertex]) {
+        if(!vis[child]) {
+            dfs(child); 
+        }
+    }
+}
+ 
+int main() {
+    ll n, m;
+    cin >> n >> m; 
+ 
+   
+    for(ll i = 0; i < m; ++i) {
+        ll v1, v2;
+        cin >> v1 >> v2;
+        g[v1].push_back(v2);
+        g[v2].push_back(v1);
+    }
+ 
+    ll cnt = 0; 
+ 
+ 
+    for(ll i = 1; i <= n; i++) {
+        if(!vis[i]) {
+            dfs(i); 
+            cnt++; 
+        }
+    }
+ 
+    cout << cnt << endl; 
+    return 0;
+}
